28097b23f172433b4b2e121e7529291e8adc8200
vjalal <vjalalvandi@gmail.com>
2023-03-04 01:17:51 -0500

    added the search bar with animation

--- MainView.tsx
+++ MainView.tsx
@@ -1,15 +1,29 @@
 import React, {useState} from 'react';
 import {
-    
+    LayoutAnimation,
     Image,
     Text,
     View,
   } from 'react-native';
 import SquareImageGrid from './ImageBox'
 
-const SearchBar = () => {
-    return;
-}
+import SearchBar from 'react-native-platform-searchbar';
+const Search = () => {
+    const [value, setValue] = useState('');
+    const [size, setSize] = useState(20);
+    
+    return(<SearchBar
+        //onPressIn={() => setSize("100%")}
+        onFocus={() => setSize(100)}
+        onBlur ={() => {LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut); setSize(20)}}
+        value={value}
+        onChangeText={setValue}
+        theme={'dark'}
+        platform={'ios'}
+        style={{width:size+'%'}}
+        inputStyle={{backgroundColor:'#946020', opacity:100}}
+    />);
+};
 const MainImages = () => {
     return(<SquareImageGrid/>);
     return (
@@ -22,7 +36,7 @@ const MainImages = () => {
 const MainView = () => {
     return(
     <View style={{backgroundColor:'#181', flex:1, }}>
-      <View  style={{backgroundColor:'#A76B09', flex:0.07,}}></View>
+      <View  style={{backgroundColor:'#181B22', flex:0.07, justifyContent:'center'}}><Search/></View>
       <View  style={{backgroundColor:'#181B22', flex:0.93,}}><MainImages/></View>
     </View>);
   }
